067c52e5d43b0b147e3f1e14ecc53c9f88b638fb
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-06-23 16:14:18 +0200

    Add SPI macro plugins.

---
+++ lua/data_provider_pt/plugins/download_spi_macro_v1.lua
@@ -0,0 +1,35 @@
+local class = require 'pl.class'
+local DataProviderDownloadBase = require 'data_provider_pt.download_base'
+local DataProviderDownloadSpiMacroV1 = class(DataProviderDownloadBase)
+
+function DataProviderDownloadSpiMacroV1:_init(tLog)
+  self:super(tLog, 'download_spi_macro_v1')
+end
+
+
+function DataProviderDownloadSpiMacroV1:getData(strItemName, tCfg)
+  local tLog = self.tLog
+  local tData
+  local strMessage
+
+  local strUrl = tCfg.URL
+
+  -- Generate a temporary filename.
+  local strTempFile = self:getTempFile(strItemName)
+
+  -- Download the definition.
+  tLog.debug('Download the definition from %s to %s.', strUrl, strTempFile)
+  local tResult, strDownloadError = self:curl_get_url_file(strUrl, strTempFile)
+  if tResult==nil then
+    strMessage = string.format('Failed to download the definition from "%s": %s', strUrl, tostring(strDownloadError))
+  else
+    tData = {
+      path = strTempFile
+    }
+  end
+
+  return tData, strMessage
+end
+
+
+return DataProviderDownloadSpiMacroV1
---
+++ lua/data_provider_pt/plugins/local_spi_macro_v1.lua
@@ -0,0 +1,33 @@
+local class = require 'pl.class'
+local PluginBase = require 'data_provider_pt.plugin_base'
+local DataProviderLocalSpiMacroV1 = class(PluginBase)
+
+function DataProviderLocalSpiMacroV1:_init(tLog)
+  self:super(tLog, 'local_spi_macro_v1')
+end
+
+
+function DataProviderLocalSpiMacroV1:getData(_, tCfg)
+  local tData
+  local strMessage
+
+  -- Check if the file exists.
+  local path = require 'pl.path'
+  local strPath = tCfg.PATH
+  if path.exists(strPath)~=strPath then
+    strMessage = string.format('The path "%s" does not exist.', strPath)
+
+  elseif path.isfile(strPath)~=true then
+    strMessage = string.format('The path "%s" does not point to a file.', strPath)
+
+  else
+    tData = {
+      path = strPath
+    }
+  end
+
+  return tData, strMessage
+end
+
+
+return DataProviderLocalSpiMacroV1
