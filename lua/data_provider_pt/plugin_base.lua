e3646113598c556d6fe7b6393ce6688e1b9bfb04
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2024-09-26 15:31:07 +0200

    Add getHashSumFromMessage, getHashForFile, getDoubleHashForFile and convertAsciiHashToBinary.

--- lua/data_provider_pt/plugin_base.lua
+++ lua/data_provider_pt/plugin_base.lua
@@ -4,6 +4,17 @@ local PluginBase = class()
 function PluginBase:_init(tLog, strID)
   self.tLog = tLog
   self.strID = strID
+
+  local mhash = require 'mhash'
+  self.atKnownHashPrefixes = {
+    SHA384 = mhash.MHASH_SHA384,
+    SHA512 = mhash.MHASH_SHA512
+  }
+
+  self.atKnownHashSizes = {
+    [mhash.get_block_size(mhash.MHASH_SHA384)] = mhash.MHASH_SHA384,
+    [mhash.get_block_size(mhash.MHASH_SHA512)] = mhash.MHASH_SHA512
+  }
 end
 
 
@@ -12,7 +23,69 @@ function PluginBase:getID()
 end
 
 
+function PluginBase:getHashSumFromMessage(strHashMessage)
+  local tLog = self.tLog
+  local mhash = require 'mhash'
+  local strHashSum
+  local tHashFormatOrErrorMsg
+
+  -- Does the hash message have a prefix separated with a colon?
+  local strHashPrefix, strHashSumClean = string.match(string.upper(strHashMessage), '^([^:]+):(%x+)')
+  if strHashPrefix~=nil then
+    -- The hash has a prefix. Look it up in the list of known Hashes.
+    local tHashAlgo = self.atKnownHashPrefixes[strHashPrefix]
+    if tHashAlgo==nil then
+      strHashSum = nil
+      tHashFormatOrErrorMsg = string.format('Unknown hash prefix "%s".', strHashPrefix)
+    else
+      -- Validate the length of the hash.
+      local sizHashSum = string.len(strHashSumClean) / 2 -- A hexdump needs 2 characters for one byte.
+      local sizHashAlgo = mhash.get_block_size(tHashAlgo)
+      if sizHashSum~=sizHashAlgo then
+        strHashSum = nil
+        tHashFormatOrErrorMsg = string.format(
+          'Invalid size of the hash sum for algo %s. Should be %d bytes, but received %d bytes.',
+          strHashPrefix,
+          sizHashAlgo,
+          sizHashSum
+        )
+      else
+        tLog.debug('Detected hash %s by prefix.', mhash.get_hash_name(tHashAlgo))
+        strHashSum = strHashSumClean
+        tHashFormatOrErrorMsg = tHashAlgo
+      end
+    end
+  else
+    -- The has has no prefix, guess from a list of known sizes.
+
+    -- Get as many hex digits from the start of the data as possible.
+    strHashSumClean = string.match(strHashMessage, '^(%x+)')
+    local sizHashSum = string.len(strHashSumClean) / 2 -- A hexdump needs 2 characters for one byte.
+    local tHashAlgo = self.atKnownHashSizes[sizHashSum]
+    if tHashAlgo==nil then
+      strHashSum = nil
+      tHashFormatOrErrorMsg = string.format(
+        'Unknown hash size %d. No algo matches.',
+        sizHashSum
+      )
+    else
+      tLog.debug('Detected hash %s by size.', mhash.get_hash_name(tHashAlgo))
+      strHashSum = strHashSumClean
+      tHashFormatOrErrorMsg = tHashAlgo
+    end
+  end
+  local strBinaryHash = self.convertAsciiHashToBinary(strHashSum)
+
+  return strBinaryHash, tHashFormatOrErrorMsg
+end
+
+
 function PluginBase:getSha384ForFile(strFilename)
+  return self:getHashForFile(require 'mhash'.MHASH_SHA384, strFilename)
+end
+
+
+function PluginBase:getHashForFile(tHashAlgo, strFilename)
   local tLog = self.tLog
 
   -- Open the file for reading.
@@ -25,7 +98,7 @@ function PluginBase:getSha384ForFile(strFilename)
 
   local mhash = require 'mhash'
   local mh = mhash.mhash_state()
-  mh:init(mhash.MHASH_SHA384)
+  mh:init(tHashAlgo)
   repeat
     local aucChunk = tFile:read(16384)
     mh:hash(aucChunk)
@@ -34,6 +107,35 @@ function PluginBase:getSha384ForFile(strFilename)
   tFile:close()
 
   return tHash
+end
+
+
+function PluginBase:getDoubleHashForFile(tHashAlgo1, tHashAlgo2, strFilename)
+  local tLog = self.tLog
+
+  -- Open the file for reading.
+  local tFile, strError = io.open(strFilename, 'r')
+  if tFile==nil then
+    local strMsg = string.format('Failed to open "%s" for reading: %s', strFilename, tostring(strError))
+    tLog.error(strMsg)
+    error(strMsg)
+  end
+
+  local mhash = require 'mhash'
+  local mh1 = mhash.mhash_state()
+  local mh2 = mhash.mhash_state()
+  mh1:init(tHashAlgo1)
+  mh2:init(tHashAlgo2)
+  repeat
+    local aucChunk = tFile:read(16384)
+    mh1:hash(aucChunk)
+    mh2:hash(aucChunk)
+  until aucChunk==nil
+  local tHash1 = mh1:hash_end()
+  local tHash2 = mh2:hash_end()
+  tFile:close()
+
+  return tHash1, tHash2
 end
 
 
@@ -50,6 +152,24 @@ function PluginBase.convertBinaryHashToAscii(strBinaryHash)
     )
   end
   return table.concat(astrHashAscii)
+end
+
+
+function PluginBase.convertAsciiHashToBinary(strAsciiHash)
+  local sizHashSumAscii = string.len(strAsciiHash)
+  local astrHashBin = {}
+  for uiPos=1,sizHashSumAscii,2 do
+    table.insert(
+      astrHashBin,
+      string.char(
+        tonumber(
+          string.sub(strAsciiHash, uiPos, uiPos+1),
+          16
+        )
+      )
+    )
+  end
+  return table.concat(astrHashBin)
 end
 
 
